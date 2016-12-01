952bf244a789eb473097ab6f20cf851df96afdf7
Tom Barber <tom@analytical-labs.com>
2016-12-01 21:51:22 +0000

    add fixes

--- reactive/solr.py
+++ reactive/solr.py
@@ -1,17 +1,28 @@
 from charms.reactive import when, when_not, set_state
+from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
+from subprocess import check_call, CalledProcessError, call, check_output
+from charmhelpers.core.hookenv import status_set, log
+from charmhelpers.core import hookenv
 
+au = ArchiveUrlFetchHandler()
 
 @when_not('solr.installed')
 def install_solr():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
+    au.download("http://apache.claz.org/lucene/solr/6.3.0/solr-6.3.0.tgz", "/tmp/solr.tgz")
+    check_output(['tar', 'xvfz', "/tmp/solr.tgz", '-C', "/opt/"])
     set_state('solr.installed')
+
+@when('solr.installed')
+@when('java.ready')
+@when_not('solr.running')
+def run_solr(java):
+   hookenv.open_port('8983')
+   check_output(['/opt/solr-6.3.0/bin/solr', 'start', '-force'])
+   status_set('running', 'Solr Running.')
+   set_state('solr.running')
+
+
+@when_not('java.ready')
+def update_java_status():
+    status_set('blocked', 'Waiting for Java.')
+
